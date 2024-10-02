ed28c3bbcbc2539f4d1ff4587835a333d9366f16
Eric <ericthered005@gmail.com>
2024-10-02 19:37:39 +0000

    added link to about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Check out the <a href="/about" class="btn">About Page</a></p>
+
+<style>
+  .btn {
+    @apply inline-block px-6 py-2.5 bg-blue-600 text-gray-200 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out no-underline cursor-default;
+  }
+</style>
